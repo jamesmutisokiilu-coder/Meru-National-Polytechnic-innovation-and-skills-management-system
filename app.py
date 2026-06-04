65eac999eb38f9333d37f3e39feb2ee396a8d90b
jamesmutisokiilu-coder <jamesmutisokiilu@gmail.com>
2026-06-05 00:09:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -61,14 +61,16 @@ cloudinary.config(
 # =========================================================
 # MODELS
 # =========================================================
+
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(200))  # CHANGE THIS BACK
-    email = db.Column(db.String(200), unique=True)
-    password = db.Column(db.String(500))
-    role = db.Column(db.String(50))
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), default="user")
+    
 
 
 class Project(db.Model):
